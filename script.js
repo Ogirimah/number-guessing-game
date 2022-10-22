685b4dc3df1cf8203c14e90795e17d8190741ae8
Ameer Ogirimah <ao22abk@herts.ac.uk>
2022-10-22 09:41:13 +0100

    Comment out some debugging codes

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // let username = prompt("Please input your username: ");
 
+let score = 0;
+let continueGame = true;
+let min = 1;
+let max = 2;
+
+do {
+    // iterate through numbers 1 upward to be stage and continue to add to score
+    // On failure print out total score
+    // ask if interested in continuing game 
+    // if affirmative, continue 
+    // else continueGame = false
+} while (continueGame == true);
+
 // function generateRandomNumber(min, max) {
 //     let RandomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 //     return RandomNumber;
@@ -17,5 +30,5 @@ function predictor(min, max) {
     return guessedNumber;
 }
 
-let b = predictor(1, 5)
-console.log(b);
+// let b = predictor(1, 5)
+// console.log(b);
